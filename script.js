9c072dcd79ab65dfa1cd44368bd568838d6e3be9
Andreas Situmorang <136316420+and123reast@users.noreply.github.com>
2025-04-14 01:29:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const departures = [
-    { time: "01:29", vessel: "Dolphin 2", destination: "Stulang Laut", status: "Scheduled" },
     { time: "06:45", vessel: "Dolphin 2", destination: "Stulang Laut", status: "Scheduled" },
     { time: "13:30", vessel: "Dolphin 5", destination: "Stulang Laut", status: "Scheduled" },
     { time: "16:45", vessel: "Dolphin 2", destination: "Stulang Laut", status: "Scheduled" }
